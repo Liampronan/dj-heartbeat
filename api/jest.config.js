92706073d50a48a3456bf7d35befe071dd54f1d8
Liam Ronan <Liampronan@gmail.com>
2024-06-21 13:41:40 -0700

    add coverage

--- api/jest.config.js
+++ api/jest.config.js
@@ -3,4 +3,5 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/tests/**/*.test.ts"],
+  collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}", "!<rootDir>/node_modules/"],
 };
