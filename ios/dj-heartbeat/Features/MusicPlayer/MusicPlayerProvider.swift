0fb29bae1fad44fe69827408be7e15c2d93ceb85
Liam Ronan <Liampronan@gmail.com>
2024-06-28 12:02:13 -0700

    initial app player functionality

--- ios/dj-heartbeat/Features/MusicPlayer/MusicPlayerProvider.swift
+++ ios/dj-heartbeat/Features/MusicPlayer/MusicPlayerProvider.swift
@@ -14,18 +14,10 @@ protocol MusicPlayerProvider {
     func queueItemsFromTestPlaylist() async throws
 }
 
-@Observable
-class ObservableState<Item> {
-    var item: Item
-
-    init(item: Item) {
-        self.item = item
-    }
-}
-
 @Observable class AppleMusicPlayer: NSObject, MusicPlayerProvider {
     var playedItems = [String]()
     var playbackStatus: MusicKit.MusicPlayer.PlaybackStatus = ApplicationMusicPlayer.shared.state.playbackStatus
+    private var appMusicPlayer: ApplicationMusicPlayer = ApplicationMusicPlayer.shared
     
     private var subscriptions = Set<AnyCancellable>()
     
@@ -40,10 +32,11 @@ class ObservableState<Item> {
     
     var currentSongTitle: String = ""
     
-    private var appMusicPlayer: ApplicationMusicPlayer = ApplicationMusicPlayer.shared
-    
     func play() {
-        Task { try? await ApplicationMusicPlayer.shared.play() }
+        Task {
+            try? await ApplicationMusicPlayer.shared.play()
+            print("queue", appMusicPlayer.queue.entries.count)
+        }
     }
     
     func pause() {
@@ -59,50 +52,24 @@ class ObservableState<Item> {
     }
     
     func queueItemsFromTestPlaylist() async throws {
-        let album = try await testFindAlbum()
-        print(album)
-        print("album^^^")
-        appMusicPlayer.queue = [album]
+        let playlistSongs = try await testingFindPlaylistTracks()
+      
+        let entry = MusicPlayer.Queue.Entry(playlistSongs.last!)
+        // note: we must have a queue with an entries and call `prepareToPlay` BEFORE adding to the queue
+        appMusicPlayer.queue = .init([entry])
+        try await appMusicPlayer.prepareToPlay()
         
-        
-        
-        /*
-         let tracks = try await testingFindPlaylistTracks()
-//        appMusicPlayer.queue = tracks
-        
-        let collection = MusicItemCollection(tracks)
-        
-//        ApplicationMusicPlayer.shared
-        try await appMusicPlayer.queue.insert(collection, position: .tail)
-//        try await  appMusicPlayer.skipToNextEntry()
-//        appMusicPlayer.queue = tracks
-        print(appMusicPlayer.queue)
-        print("queue", appMusicPlayer.queue.entries.count)
-        try await ApplicationMusicPlayer.shared.prepareToPlay()
-         */
-    }
-    
-    func testFindAlbum() async throws -> Album {
-        let request = MusicCatalogResourceRequest<Album>(matching: \.id, equalTo: "1603171516")
-        let response = try await request.response()
-
-        guard let album = response.items.first else {
-            throw NSError(domain: "me", code: 404)
-        }
-          
-        let player = ApplicationMusicPlayer.shared
-
-        return album
+        let secondEntry = MusicPlayer.Queue.Entry(playlistSongs.first!)
+        try await appMusicPlayer.queue.insert(secondEntry, position: .afterCurrentEntry)
     }
     
     //https://music.apple.com/us/playlist/dj-heartbeat/pl.u-XkD0Y6pT438xpo
     let playlistId = "pl.u-XkD0Y6pT438xpo"
-    func testingFindPlaylistTracks() async throws -> [Song] {
+    func testingFindPlaylistTracks() async throws -> [Track] {
         
         // Create a request for the playlist
         var playlistRequest = MusicCatalogResourceRequest<Playlist>(matching: \.id, equalTo: MusicItemID(playlistId))
         playlistRequest.properties = [.tracks]
-        
         
         // Perform the request
         let response2 = try await playlistRequest.response()
@@ -115,15 +82,8 @@ class ObservableState<Item> {
         guard let songs = playlist.tracks?.compactMap({ $0 }) else {
             throw NSError(domain: "Playlist tracks not found", code: 404, userInfo: nil)
         }
-        let songIds = songs.map { $0.id }
-        let playlistSongsRequest = MusicCatalogResourceRequest<Song>(matching: \.id, memberOf: songIds)
-        let fetchedSongsResponse = try await playlistSongsRequest.response()
-        let fetchedSongs = fetchedSongsResponse.items.compactMap { $0 }
-//        return songs
-        return fetchedSongs
-        // Fetch the songs in the playlist
-//        let songsRequest = MusicCatalogResourceRequest<Song>(matching: \.id, in: playlist.tracks.map { $0.id })
-//        let songsResponse = try await songsRequest.response()
+
+        return songs
     }
     
     func printQueueCurrentEntry() {
